678689e018c1f2d847dfdfa1b7935e4f4bbd6b69
ole <oliver.saunders@gmail.com>
2008-12-02 13:50:31 -0500

    Using Aquarium for AOP. Got one thing working but I think there is a shorter syntax which I am going to try now.

--- lib/suprdate/occurrence_spec.rb
+++ lib/suprdate/occurrence_spec.rb
@@ -1,8 +1,37 @@
+require 'rubygems'
+require 'aquarium'
+
 module Suprdate
   
   class OccurrenceSpec
     
+    # used to specify which methods of this class automatically delimit the current sentence
+    def self.delimiter_methods(*methods)
+      methods.each do |method|
+        aliased_method = method.to_s + '_not_delimited'
+        alias_method aliased_method, method
+        private aliased_method
+        define_method(method) do |*args|
+          delimit
+          send(aliased_method, *args)
+        end
+      end
+    end
+    
+    def self.self_returners(*methods)
+      methods.each do |method|
+        aliased_method = method.to_s + '_not_returning_self'
+        alias_method aliased_method, method
+        private aliased_method
+        define_method(method) do |*args|
+          send(aliased_method, *args)
+          self
+        end
+      end
+    end
+    
     attr_accessor :name, :builder, :range_factory
+    attr_reader :sentences
     
     def initialize(name = 'Chunky bacon', builder = nil)
       @name = name
@@ -16,7 +45,6 @@ module Suprdate
       @to = Inf 
       @from = nil
       @freq = freq
-      self
     end
     
     {:years => :year, :months => :month, :days => :day}.each_pair do |plural, singular|
@@ -24,42 +52,41 @@ module Suprdate
       define_method(singular) { @unit = singular; self }
     end
     
-    def self.delimit_with(*methods)
-      
-    end
-    
-    delimit_with :sentences, :occurrences
-    
     def occurrences
-      self.and
       Suprdate.every(@freq, last_range.to_a)
     end
     
-    def and
+    def delimit
       @sentences << {:freq => @freq, :unit => @unit, :to => @to, :from => @from} unless @stored
       @stored = true
-      self
     end
     
     def from(*from)
       @from = builder.date(*from)
-      self
     end
     
     def to(*to)
       @to = builder.date(*to)
-      self
     end
     
     def last_range
       @range_factory.new(@from.send(@unit), @to)
     end
     
-    def sentences
-      self.and
-      @sentences
+    def happens
+      self
     end
     
+    #delimiter_methods :sentences, :occurrences, :last_range
+    self_returners :to, :from, :delimit, :every
+    alias :and :delimit
+    
+  end
+  
+  Aquarium::Aspects::Aspect.new :before, 
+    :calls_to => [:sentences, :occurrences, :last_rance], 
+    :for_type => OccurrenceSpec do |join_point, obj, sym, *args|
+    obj.delimit  
   end
   
 end
